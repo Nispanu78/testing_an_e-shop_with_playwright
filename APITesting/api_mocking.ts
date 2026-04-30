62599fecf88bfd19203785e2f76e93f5da07ddf4
Nicola Spanu <nicspanu@gmail.com>
2026-04-30 11:08:52 +0200

    api_testing_with_playwright

---
+++ APITesting/api_mocking.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test"
+
+const products = [{
+  product: "abc",
+  quantity: 1,
+  price: 10
+}, {
+  product: "def",
+  quantity: 2,
+  price: 20
+}]
+
+test("Populate the database with mocked data and retrieve it", async({ page }) => {
+  const intercepted = false;
+  await page.route("**/api/products", route => {
+    const intercepted = true;
+    route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify(products)
+    });
+  });
+
+  await page.goto("/products")
+  expect(intercepted).toBeTruthy();
+  await expect(page.locator(".products")).toHaveCount(2);
+  await expect(page.getByText("abc")).toBeVisible();
+  await expect(page.getByText("def")).toBeVisible();
+})

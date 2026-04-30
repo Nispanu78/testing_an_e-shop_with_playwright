7e65598083e7d9c4ee657aadbd3a0929fd808d81
Nicola Spanu <nicspanu@gmail.com>
2026-04-30 14:51:37 +0200

    corrections

--- APITesting/api_testing.ts
+++ APITesting/api_testing.ts
@@ -5,38 +5,34 @@ type CreateUser = {
     surname: string
     email: string
     profession: string
-
 }
 
 type User = CreateUser&{
     id: number
 }
 
-test("Create and retrieve a user", async({ request }) => {
+test("Create and retrieve user", async({ request }) => {
     const payload: CreateUser = {
         name: "John",
-        surname: "Dandy",
-        email: "dandy@mail.com",
+        surname: "Doe",
+        email: "doe@mail.com",
         profession: "tester"
     }
 
-    const response = await request.post("/api/users", {
+    const createdUser = await request.post('/api/users', {
         data: payload
-    })
+    });
 
-    expect(response.status()).toBe(201)
+    expect(createdUser.status()).toBe(201);
+    const createdUserPost: User = await createdUser.json();
+    expect(createdUserPost.id).toBeGreaterThan(0);
+    expect(createdUserPost).toMatchObject(payload);
 
-    const created: User = await response.json()
-    expect(created.id).toBeGreaterThan(0)
-    expect(created).toMatchObject(payload)
-
-    const getUser = await request.get(`/api/users/${created.id}`)
-    expect(getUser.status()).toBe(200)
-
-    const fetched: User = await getUser.json()
-    expect(fetched).toMatchObject({
-        id: created.id,
+    const getUser = await request.get(`/api/users/${createdUserPost.id}`);
+    expect(getUser.status()).toBe(200);
+    const getUserGet: User = await getUser.json();
+    expect(getUserGet).toMatchObject({
+        id: getUserGet.id,
         ...payload
     })
-    }
-)
+})

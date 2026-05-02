beb1e17b3f3d88b4a2d4574a7070f2802d59df35
Nicola Spanu <nicspanu@gmail.com>
2026-05-02 13:03:24 +0200

    corrections

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,35 +1,6 @@
-// import { Page, Locator } from "@playwright/test"
-// import { loginLocators } from "../locators/login.locators.ts"
-
-
-// export class Login {
-
-//     readonly page: Page
-//     readonly userInput: Locator
-//     readonly passwordInput: Locator
-//     readonly loginButton: Locator
-
-//     constructor(page: Page) {
-//         this.page = page
-//         this.userInput = page.getByPlaceholder(loginLocators.usernmameField)
-//         this.passwordInput = page.getByPlaceholder(loginLocators.passwordField)
-//         this.loginButton = page.getByRole(loginLocators.loginButton.role, loginLocators.loginButton.options)
-//     }
-
-//     async goTo(): Promise<void>{
-//         await this.page.goto(loginLocators.baseUrl)
-//     }
-
-//     async loginWithCorrectCredentials(username: string, password:string): Promise<void>{
-//         await this.userInput.fill(username)
-//         await this.passwordInput.fill(password)
-//         await this.loginButton.click()
-//     }
-// }
-
 import { Page, Locator } from "@playwright/test"
 import { loginLocators } from "../locators/login.locators"
-import { testData } from "../test-data/data"
+
 
 export class Login {
 

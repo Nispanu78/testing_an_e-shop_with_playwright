b0364a062b915abf1399b1910dd6a18172a336d7
Nicola Spanu <nicspanu@gmail.com>
2026-05-04 09:39:33 +0200

    corrections

--- tests/valid_login_with_fixture.spec.ts
+++ tests/valid_login_with_fixture.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "@playwright/test"
 import { valid_login } from "../fixtures/login.fixture.ts"    
-import { Login } from "../pages/login.page.ts";
+
 
 valid_login("Login with correct credentials", async({ authenticatedLogin }) => {
     await expect(authenticatedLogin).toHaveURL('https://www.saucedemo.com/inventory.html');
